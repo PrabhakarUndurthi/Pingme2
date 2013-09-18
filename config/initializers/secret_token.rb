e6f3d1cb48330848982a2ac8661289eb5fc2096a
prabhakar <undurthi.prabhakar@gmail.com>
2013-09-19 01:21:29 +0530

    Created a rails application , and commited to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingme::Application.config.secret_token = '08b9d2305077ce3e76bca268323e6b79a484556e812efb530955cfd1d971909aecd827679239be2dfbd2a846fc7f60d0481532642e478ba6f785cb055aa48bf6'
