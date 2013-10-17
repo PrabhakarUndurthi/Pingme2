b1b6beaa663224107d2d10e8b1e10b53e92eabae
prabhakar <undurthi.prabhakar@gmail.com>
2013-10-17 23:17:05 +0530

    Performed test cases on the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+  validates :first_name, presence:true
+  validates :last_name,presence:true
+  validates :profile_name,presence:  true,
+                          uniqueness:  true
+
 
   has_many :statuses
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+	test  "a.user should enter a.first name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end	
+  
+
+
+  test  "a.user should enter a.last name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end	
+
+
+
+	test  "a.user should enter a.profile name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end	
+
+
+	test  "a.user should enter a.unique profile name" do 
+		user = User.new
+		user.profile_name = "Prabhakar Undurthi"
+		user.email =   "undurthi_prabhakar@aol.com"
+		user.first_name = "Prabhakar"
+		user.last_name = "Undurthi"
+		user.password ="password"
+		user.password_confirmation = "password"
+		assert !user.save
+		assert !user.errors[:profile_name] .empty?
+	   end
+	end	
+
