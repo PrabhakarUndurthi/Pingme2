7e528c77306086600bc90608a2c17bf16363b0e6
prabhakar <undurthi.prabhakar@gmail.com>
2013-10-17 19:31:47 +0530

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- db/migrate/20130919054913_devise_create_users.rb
+++ db/migrate/20130919054913_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :profile_name
+      
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
