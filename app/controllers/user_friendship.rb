9f2a7fa21575626e9fd8e93b54fbb1ef7ee5ffe6
prabhakar <undurthi.prabhakar@gmail.com>
2014-11-18 15:25:03 +0530

    Update user_friendship.rb

--- app/controllers/user_friendship.rb
+++ app/controllers/user_friendship.rb
@@ -9,47 +9,44 @@ class UserFriendship < ActiveRecord::Base
 
 	state_machine :state, initial: :pending do
 
-		after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship!]
+	   after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship!]
 
-		state :requested	
-		event :accept do
-			transition any => :accepted
-		end
+	    state :requested	
+	    event :accept do
+	  transition any => :accepted
 	  end
+       end
 
-	  def self.request(user1, user2)
-	  	transaction do
-	  	  friendship1 = create!(user:user1 , friend:user2 , state: 'pending')
-	  	  friendship2 = create!(user: user2, friend:user1, state: 'requested')
+     def self.request(user1, user2)
+  	transaction do
+  	  friendship1 = create!(user:user1 , friend:user2 , state: 'pending')
+  	  friendship2 = create!(user: user2, friend:user1, state: 'requested')
 
-	  	  friendship1.send_request_email
-	  	  friendship1
-	  end
-	end
+  	  friendship1.send_request_email
+  	  friendship1
+	 end
+      end
 
      # Send the  friend request email to the user.
      # Example: 1 Friend request notification.
 	def send_request_email
-		UserNotifier.friend_requested(id).deliver
+	  UserNotifier.friend_requested(id).deliver
 	end
 
       # After accepting the friend request , it would gives a nice message to the
 	    #user by saying your request has been accpeted.
 
 	def send_acceptence_email
-		UserNotifier.friend_request_accepted(id).deliver
+	  UserNotifier.friend_request_accepted(id).deliver
 	end
 
 	def mutual_friendship
-		self.class.where({user_id: friend_id: user_id}).first
+	  self.class.where({user_id: friend_id: user_id}).first
 	end
 
 
- def accept_mutual_friendship!
- 	# Grab the mutual friendship ans update the state without using
- 	# the state machine so as not to invoke callbacks.
- 	mutual_friendship = self.class.where({user_id: friend_id, friend_id:user_id}).friend
- 	mutual_friendship.update_attribute(:state, 'accpeted')
+    def accept_mutual_friendship!
+     mutual_friendship = self.class.where({user_id: friend_id, friend_id:user_id}).friend
+     mutual_friendship.update_attribute(:state, 'accpeted')
+   end
  end
-
-end
